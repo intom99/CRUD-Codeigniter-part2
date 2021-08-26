f19cb02abbd8459df7210b3dd1680d74a926ce94
intom <iin.arahmah99@gmail.com>
2021-08-27 02:03:13 +0700

    update file db

--- tb_mahasiswa.sql
+++ tb_mahasiswa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 02 Okt 2020 pada 18.27
+-- Waktu pembuatan: 26 Agu 2021 pada 21.01
 -- Versi server: 10.4.11-MariaDB
 -- Versi PHP: 7.4.4
 
@@ -44,12 +44,12 @@ CREATE TABLE `tb_mahasiswa` (
 --
 
 INSERT INTO `tb_mahasiswa` (`id`, `nama`, `nim`, `tgl_lahir`, `jurusan`, `alamat`, `email`, `no_telp`, `foto`) VALUES
-(4, 'Ravania Nirvana', 1234250, '2000-01-10', 'Informatika', 'Jl. Disitu', 'iin.arahmah02@gmail.com', '0832232342', '0'),
+(4, 'Ravania Nirvana', 1234250, '2000-01-10', 'Informatika', 'Jl. Disitu aja', 'iin.arahmah02@gmail.com', '0832232342', '41ffd315-09cc-4c37-830d-ce51f8183716.jpeg'),
 (7, 'Rachel Michael', 5123135, '1997-03-12', 'Sistem Komputer', 'Jl. Kemarin', 'p23onestoprental@gmail.com', '083243242', '0'),
 (10, 'Kenanga Flora', 5301112, '2003-10-10', 'Teknik Mesin', 'Jl. Langgih', 'kflora@gmail.com', '0832232342', '0'),
 (11, 'Michael Angelo', 4323479, '2002-03-14', 'Informatika', 'Jl. Merdeka no.17', 'p24onestoprental@gmail.com', '0832232341', '0'),
-(12, 'Erlangga', 3422532, '2000-11-22', 'Manajemen Informatika', 'Jl. Kemana', 'iin.arahmah@gmail.com', '082342234', '0'),
-(13, 'Indah Sari', 2147483647, '2001-02-01', 'Informatika', 'Jl. Dimana', 'arahmah.iin@gmail.com', '0832232341', 'example.png'),
+(12, 'Dwi Erlangga', 3422532, '2000-11-22', 'Manajemen Informatika', 'Jl. Kemana itu 123', 'erlangga@gmail.com', '082342234', 'firework-2.jpg'),
+(13, 'Indah Sari', 2147483647, '2001-02-01', 'Informatika', 'Jl. Dimana 123', 'arahmah.iin@gmail.com', '0832232341', 'bc4895c2b9d4549de4f98d618a15f550.jpg'),
 (14, 'Gladisya Azzellea', 2147483647, '2000-08-09', 'teknik mesin', 'Jl. Talaga', 'gladisya.azz@gmail.com', '089832781310', '2.jpg');
 
 --
@@ -70,7 +70,7 @@ ALTER TABLE `tb_mahasiswa`
 -- AUTO_INCREMENT untuk tabel `tb_mahasiswa`
 --
 ALTER TABLE `tb_mahasiswa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
